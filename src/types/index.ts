4dfb009c6e6fc782243161b96113ed1cf9277478
Bigzawg12 <seventhnavalfleet@gmail.com>
2025-08-11 00:51:55 -0400

    Updated index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@
    id: string;
    title: string;
    description: string;
+ }
 -  type: 'xp' | 'time' | 'workout_count' | 'streak';
 +  type: 'xp' | 'time' | 'workout_count' | 'streak' | 'distance';
    target: number;
